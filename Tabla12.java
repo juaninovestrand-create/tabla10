08473beedda90dc688a9167707c60b1aa53fc417
Roberto <juan@hotmail.com>
2026-06-15 21:58:21 +0200

    Añadida tabla del 12

---
+++ Tabla12.java
@@ -0,0 +1,7 @@
+public class Tabla12 {
+    public static void main(String args[]) {
+        for(int i = 0; i <= 10; i++) {
+            System.out.println("12 * " + i + " = " + 12 * i + "\n");
+        }
+    }
+}
