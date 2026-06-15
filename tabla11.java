23408dcfeeb9cdb9d1766bbcd09a16bd3d44c983
juaninovestrand-create <juaninovestrand@gmail.com>
2026-06-15 22:02:25 +0200

    Change loop limit from 10 to 20 in Tabla11

--- tabla11.java
+++ tabla11.java
@@ -1,6 +1,6 @@
 public class Tabla11 {
     public static void main(String args[]) {
-        for(int i = 0; i <= 10; i++) {
+        for(int i = 0; i <= 20; i++) {
             System.out.println("11 * " + i + " = " + 11 * i + "\n");
         }
     }
